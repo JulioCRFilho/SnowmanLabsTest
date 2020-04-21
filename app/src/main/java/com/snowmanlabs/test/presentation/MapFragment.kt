da3ae9babffd0093409ff141f7bd4a211a54f367
Julio Cesar <julio.filho@devmaker.com.br>
2020-04-20 21:37:47 -0300

    added current location and data binding

--- app/src/main/java/com/snowmanlabs/test/presentation/MapFragment.kt
+++ app/src/main/java/com/snowmanlabs/test/presentation/MapFragment.kt
@@ -1,26 +1,33 @@
 package com.snowmanlabs.test.presentation
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.snowmanlabs.test.R
+import com.snowmanlabs.test.databinding.FragmentMapBinding
 import com.snowmanlabs.test.utils.bottomBar
+import com.snowmanlabs.test.viewModel.MapViewModel
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var mMap: GoogleMap
+    private lateinit var viewModel: MapViewModel
+    private lateinit var binding: FragmentMapBinding
 
     override fun onCreateView(i: LayoutInflater, v: ViewGroup?, s: Bundle?): View? {
         bottomBar(true)
-        return i.inflate(R.layout.fragment_map, v, false)
+        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        binding = DataBindingUtil.inflate(i, R.layout.fragment_map, v, false)
+        binding.viewModel = viewModel
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,9 +38,15 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        viewModel.getLocationPermission(this, mMap)
+    }
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onRequestPermissionsResult(r: Int, p: Array<out String>, g: IntArray) {
+        super.onRequestPermissionsResult(r, p, g)
+        if (r == viewModel.ACCESS_CODE) {
+            if (g.count() > 0 && g[0] == PackageManager.PERMISSION_GRANTED) {
+                activity?.let { viewModel.getInitialMark(it, mMap) }
+            }
+        }
     }
 }
--- app/src/main/java/com/snowmanlabs/test/viewModel/MapViewModel.kt
+++ app/src/main/java/com/snowmanlabs/test/viewModel/MapViewModel.kt
@@ -1,7 +1,46 @@
 package com.snowmanlabs.test.viewModel
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.util.Log.d
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapViewModel : ViewModel() {
+    val ACCESS_CODE = 101
 
+    fun getLocationPermission(fragment: Fragment, mMap: GoogleMap) {
+        if (ContextCompat.checkSelfPermission(
+                fragment.requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            getInitialMark(fragment.requireActivity(), mMap)
+        } else {
+            fragment.requestPermissions(
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_CODE
+            )
+        }
+    }
+
+    fun getInitialMark(activity: Activity, mMap: GoogleMap) {
+        val location = LocationServices.getFusedLocationProviderClient(activity).lastLocation
+        location.addOnCompleteListener {
+            if (it.isSuccessful) {
+                it.result?.let { result ->
+                    val myLoc = LatLng(result.latitude, result.longitude)
+                    mMap.addMarker(MarkerOptions().position(myLoc).title("You're here!"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLoc, 15f))
+                }
+            }
+        }
+    }
 }
